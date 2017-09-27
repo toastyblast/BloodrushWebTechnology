437c59c4de9be0a44344dd1defb8b5717daa1882
toastyblast <yorankerbusch@gmail.com>
2017-09-27 21:45:34 +0200

    Added all missing text, CSS tags and hidden FAQ items that use javascript to be shown.

--- .idea/javascriptKOI.js
+++ .idea/javascriptKOI.js
@@ -1,1 +1,14 @@
-/* Javascript... */
+function doFAQFunction() {
+    var faquestions = document.getElementById("hiddenDiv");
+    var faqButton = document.getElementsByClassName("faqbutton");
+
+    if (faquestions.style.display === 'none') {
+        faquestions.style.display = 'block';
+        faqButton.value = 'Show less questions and answers.';
+    } else {
+        faquestions.style.display = 'none';
+        faqButton.value = 'Show all questions and answers!';
+    }
+}
+
+/* More javascript... */
