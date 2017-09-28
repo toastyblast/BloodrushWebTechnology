66bf9ebace22dad6bad43530613115eeb79405d7
Martin <mslavinovslavov@gmail.com>
2017-09-28 11:30:16 +0200

    JS added

--- .idea/javascriptKOI.js
+++ .idea/javascriptKOI.js
@@ -1,3 +1,4 @@
+
 function doFAQFunction() {
     var faquestions = document.getElementById("hiddenDiv");
     var faqButton = document.getElementsByClassName("faqbutton");
@@ -12,3 +13,59 @@ function doFAQFunction() {
 }
 
 /* More javascript... */
+
+function formValidation() {
+
+    var nameToValdate = document.forms["contactForm"]["firstname"].value;
+    var emailToValidate = document.forms["contactForm"]["email"].value;
+    var subjectToValidate = document.forms["contactForm"]["subject"].value;
+    var messageToValidate = document.forms["contactForm"]["message"].value;
+    var messageToDisplay = "";
+    var list = document.createElement("ul");
+    list.className = "rightPic";
+    list.setAttribute("id", "eyy");
+    var listitem;
+    var text;
+
+    if (nameToValdate == "") {
+        messageToDisplay = ("Please fill in your name.")
+        appendListItem()
+    }
+
+    if (emailToValidate == "") {
+        messageToDisplay = "Please fill in your email adress."
+        appendListItem()
+    }
+
+    if (subjectToValidate == "") {
+        messageToDisplay = "Please fill in the subject of the email."
+        appendListItem()
+    }
+
+    if (messageToValidate == "") {
+        messageToDisplay = "Please fill in a message."
+        appendListItem()
+    }
+
+    var warningList = document.getElementById("eyy")
+    var element = document.getElementById("contactDiv");
+
+    if (warningList == null) {
+        element.appendChild(list);
+    } else {
+        element.removeChild(warningList);
+        element.appendChild(list);
+    }
+
+    if (messageToDisplay != "") {
+        return false;
+    }
+
+    function appendListItem() {
+        listitem = document.createElement("li");
+        text = document.createTextNode(messageToDisplay);
+        listitem.appendChild(text);
+        list.appendChild(listitem);
+    }
+}
+
